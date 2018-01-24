ee05e2dc09520a47ca20cc733770b14801a1b89a
韩博文 <hanbowen@meituan.com>
2018-01-25 01:49:47 +0800

    add more device models

--- swjsq.py
+++ swjsq.py
@@ -52,9 +52,17 @@ ipk_file = 'swjsq_0.0.1_all.ipk'
 log_file = 'swjsq.log'
 
 login_xunlei_intv = 600 # do not login twice in 10min
-DEVICES = ['Xiaomi MI 6', 'Huawei BLN-AL20 Honor 6X', 'Huawei EVA-AL00 P9 Premium Edition',
-'Huawei EVA-L09 P9 Standard Edition','Huawei WAS-AL00 Honor 8 Lite','LeTV Le X620 Le 2',
-'OPPO 6607 U3','Vivo X9','Vivo Xplay6','Xiaomi Mi 5X']
+DEVICES = ['Xiaomi MI 6' ,'Xiaomi Mi 5X','Xiaomi Redmi Note 3',
+'Xiaomi Redmi Note 4X','Xiaomi MIX 2 Mi Mix 2',
+'Huawei BLN-AL20 Honor 6X',
+'Huawei NXT-AL10 Mate 8','Huawei ALP-L29 Mate 10' ,'Huawei LON-AL00 Mate 9',
+'Huawei EVA-L09 P9 Standard Edition','Huawei EVA-AL00 P9 Premium Edition',
+'Huawei WAS-AL00 Honor 8 Lite', 'Huawei BLN-TL10 Honor 6X',
+'Lenovo ZUK Z2121 ZUK Z2 Pro',
+'LeTV Le X620 Le 2',
+'OPPO 6607 U3','OPPO R9s Plus'
+'Samsung SM-G9350 Galaxy S7 Edge', 'Samsung SM-N9200 Galaxy Note 5', 
+'Vivo X9s Plus','Vivo X9','Vivo Xplay6','Vivo Y927']
 OS_VERSIONS = ['7.0.1', '6.0.1', '5.1.1', '5.0.1']
 
 DEVICE = random.choice(DEVICES)
